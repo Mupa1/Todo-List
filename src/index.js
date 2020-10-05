b4f00b06329ce772445be7d3e9679294ef0a7ad2
Mupa1 <mupasmail@gmail.com>
2020-10-05 14:33:30 +0200

    create initial ui structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Initial Setup!');
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+import render from './render';
+// import project from './project';
+
+const content = document.querySelector('#content');
+// const projects = document.querySelector('#projects');
+
+content.appendChild(render());
